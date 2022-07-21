b972563ee3b2b5f1557cfe762147267b1c2a9505
Fernweh99 <daniele.dimento@gmail.com>
2022-07-21 16:25:53 +0200

    finish bonus

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
     <Title/>
+    <Jumbotron/>
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
+import Jumbotron from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
     Title,
+    Jumbotron,
 }
 }
 </script>
-
 <style>
-
+  body {
+    text-align: center;
+    background-color: darkslateblue;
+  }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="jumbotron">
+      
+    </div>
+</template>
+<script>
+export default {
+  name: "JumbotronApp",
+};
+</script>
+<style>
+  .jumbotron {
+    height: 500px;
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    background-image: url(https://picsum.photos/1000/500?random=1);
+  }
+</style>
